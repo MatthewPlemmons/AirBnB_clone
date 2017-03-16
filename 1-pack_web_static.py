4803f80b00aff3f1c639b10d6b4a00e318e9aac4
Matthew Plemmons <matthewleeplemmons@gmail.com>
2017-03-16 01:58:26 -0500

    added fabfile

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    local('mkdir -p versions')
+    time = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
+    archive = 'versions/web_static_{}.tgz'.format(time)
+    try:
+        local('tar -cvzf {} web_static'.format(archive))
+    except:
+        return None
+    return archive
