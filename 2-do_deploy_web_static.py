0d286245e6e58aa7f78b6cc7f3ee291857da6912
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-03-19 01:45:27 +0000

    use sudo to run commands

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,6 +4,7 @@ from time import strftime
 import os.path
 
 env.hosts = ['54.164.149.121', '52.201.184.69']
+env.user = 'ubuntu'
 
 
 def do_pack():
@@ -27,14 +28,14 @@ def do_deploy(archive_path):
         basename = os.path.basename(archive_path)
         archive_name = os.path.splitext(basename)[0]
         put("{}".format(archive_path), "/tmp/{}".format(basename))
-        run("mkdir -p /data/web_static/releases/{}".format(archive_name))
+        sudo("mkdir -p /data/web_static/releases/{}".format(archive_name))
         with cd("/data/web_static/releases"):
-            run("tar -xzf /tmp/{} -C {}".format(basename, archive_name))
-            run("rm /tmp/{}".format(basename))
-            run("mv {}/web_static/* {}".format(archive_name, archive_name))
-            run("rm -rf {}/web_static".format(archive_name))
-            run("rm -rf ../current")
-            run("ln -s {} ../current".format(archive_name))
+            sudo("tar -xzf /tmp/{} -C {}".format(basename, archive_name))
+            sudo("rm /tmp/{}".format(basename))
+            sudo("mv {}/web_static/* {}".format(archive_name, archive_name))
+            sudo("rm -rf {}/web_static".format(archive_name))
+            sudo("rm -rf ../current")
+            sudo("ln -s {} ../current".format(archive_name))
         print("New version deployed!")
     except Exception as e:
         print("Error: {}\nUnable to deploy new version.".format(e))
